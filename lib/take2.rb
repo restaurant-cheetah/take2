d8468d5c958bf8c76bd2748a0485c3c6a106d3e2
Anton Magids <evilnomadx@gmail.com>
2018-09-12 16:45:24 +0300

    extract code and rename main mudule

---
+++ lib/take2.rb
@@ -0,0 +1,103 @@
+require 'take2/configuration'
+
+module Take2
+
+  def self.included(base)
+    base.extend ClassMethods
+    base.send :set_defaults
+    base.send :include, InstanceMethods
+  end
+
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.reset(options = {})
+    @configuration = Configuration.new(options)
+  end
+
+  def self.configure
+    yield(configuration) if block_given?
+  end
+
+  module InstanceMethods
+
+    def call_api_with_retry
+      config = self.class.retriable_configuration
+      tries ||= config[:retries]
+      begin
+        yield
+      rescue => e
+        if config[:retriable].map {|klass| e.class <= klass }.any?
+          unless tries.zero? || config[:retry_condition_proc]&.call(e)
+            config[:retry_proc]&.call(e, tries)
+            sleep(config[:time_to_sleep]) if config[:time_to_sleep]
+            tries -= 1
+            retry
+          end
+        end
+        log_error e
+        raise e
+      end
+    end
+
+    def log_error(error)
+      Rails.logger.warn error
+    end
+  end
+
+  module ClassMethods
+
+    def number_of_retries(num)
+      self.retries = num
+    end
+
+    def retriable_errors(*errors)
+      self.retriable = errors
+    end
+
+    def retriable_condition(proc)
+      self.retry_condition_proc = proc
+    end
+
+    def on_retry(proc)
+      self.retry_proc = proc
+    end
+
+    def sleep_before_retry(seconds)
+      self.time_to_sleep = seconds
+    end
+
+    def retriable_configuration
+      Take2::Configuration::CONFIG_ATTRS.each_with_object({}) do |key, hash|
+        hash[key] = send(key)
+      end
+    end
+
+    private
+
+    attr_accessor(*Take2::Configuration::CONFIG_ATTRS)
+
+    def set_defaults
+      config = Retriable.configuration.to_hash
+      Take2::Configuration::CONFIG_ATTRS.each do |attr|
+        instance_variable_set("@#{attr}", config[attr])
+      end
+    end
+
+    def response_status(response)
+      return response.status if response.respond_to? :status
+      response.status_code if response.respond_to? :status_code
+    end
+
+    def log_client_error(error, tries)
+      Rails.logger.warn "#{self.name} - Retrying.. #{tries} of #{self.retries} (#{error})"
+    end
+
+  end
+
+end
---
+++ lib/take2/configuration.rb
@@ -0,0 +1,49 @@
+module Take2
+  class Configuration
+    CONFIG_ATTRS = [:retries, :retriable, :retry_proc, :retry_condition_proc, :time_to_sleep].freeze
+    attr_accessor(*CONFIG_ATTRS)
+
+    def initialize(options = {})
+      # Defaults
+      @retries = 3
+      @retriable =  [
+        Net::HTTPServerError,
+        Net::HTTPServerException,
+        Net::HTTPRetriableError,
+        Timeout::Error,
+        Errno::ECONNRESET,
+        IOError,
+        Faraday::ConnectionFailed,
+        HTTParty::Error,
+        Twilio::REST::RestError
+       ].freeze
+      @retry_proc = proc { |error, tries| log_client_error error, tries }
+      @retry_condition_proc = proc { false }
+      @time_to_sleep = 3
+      # Overwriting the defaults
+      options.each do |k, v|
+        raise ArgumentError, "#{k} is not a valid configuration"  unless CONFIG_ATTRS.include?(k)
+        raise ArgumentError, "#{k} must be positive integer"      unless v.is_a?(Integer) && v.positive?
+        raise ArgumentError, "#{k} must be positive number"       unless (v.is_a?(Integer) || v.is_a?(Float)) && v.positive?
+        instance_variable_set(:"@#{k}", v)
+      end
+    end
+
+    def to_hash
+      CONFIG_ATTRS.each_with_object({}) do |key, hash|
+        hash[key] = public_send(key)
+      end
+    end
+
+    def [](value)
+      self.public_send(value)
+    end
+
+    private
+
+    def log_client_error(error, tries)
+      Rails.logger.warn "#{self.name} - Retrying.. #{tries} of #{self.retries} (#{error})"
+    end
+
+  end
+end
