b4933499db3c700117fd4e6b758ea6b94f695e77
Anton Magids <evilnomadx@gmail.com>
2019-01-09 23:18:42 +0200

    rubocop corrections

--- spec/take2/configuration_spec.rb
+++ spec/take2/configuration_spec.rb
@@ -1,124 +1,95 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-RSpec.describe Take2::Configuration do
-
+RSpec.describe(Take2::Configuration) do
   describe 'default configurations' do
-
     let(:default) { described_class.new }
 
     it 'has correct default value for retries' do
-      expect(default.retries).to eql 3
+      expect(default.retries).to(eql(3))
     end
 
     it 'has correct default retriable errors array' do
-      expect(default.retriable).to eql [      
+      expect(default.retriable).to(eql([
         Net::HTTPServerException,
-        Net::HTTPRetriableError,        
+        Net::HTTPRetriableError,
         Errno::ECONNRESET,
         IOError,
-       ].freeze
+      ].freeze))
     end
 
     it 'has default proc for retry_proc' do
       p = proc {}
-      expect(default.retry_proc.call).to eql p.call
+      expect(default.retry_proc.call).to(eql(p.call))
     end
 
     it 'has default proc for retry_condition_proc' do
-      p = proc {false}
-      expect(default.retry_condition_proc.call).to eql p.call
+      p = proc { false }
+      expect(default.retry_condition_proc.call).to(eql(p.call))
     end
 
     it 'has correct default value for time_to_sleep' do
-      expect(default.time_to_sleep).to eql 3
+      expect(default.time_to_sleep).to(eql(3))
     end
-
   end
 
   describe 'overwriting the default configurations' do
-
     context 'with valid hash' do
-
-      let!(:new_configs_hash) { 
+      let!(:new_configs_hash) do
         {
           retries: 2,
           retriable: [Net::HTTPRetriableError],
           retry_condition_proc: proc { true },
-          retry_proc: proc { 2*2 },
-          time_to_sleep: 0
+          retry_proc: proc { 2 * 2 },
+          time_to_sleep: 0,
         }
-      }
+      end
 
       let!(:new_configuration) { described_class.new(new_configs_hash).to_hash }
 
       [:retries, :retriable, :retry_proc, :retry_condition_proc, :time_to_sleep].each do |key|
         it "sets the #{key} key" do
           if new_configs_hash[key].respond_to?(:call)
-            expect(new_configuration[key].call).to eql new_configs_hash[key].call
-          else  
-            expect(new_configuration[key]).to eql new_configs_hash[key]
+            expect(new_configuration[key].call).to(eql(new_configs_hash[key].call))
+          else
+            expect(new_configuration[key]).to(eql(new_configs_hash[key]))
           end
         end
       end
-
     end
 
     context 'with invalid hash' do
-
       context 'when retries set to invalid value' do
-
         it 'raises ArgumentError' do
-
-          expect { described_class.new(retries: -1) }.to raise_error ArgumentError
-          expect { described_class.new(retries: 0) }.to raise_error ArgumentError
-
+          expect { described_class.new(retries: -1) }.to(raise_error(ArgumentError))
+          expect { described_class.new(retries: 0) }.to(raise_error(ArgumentError))
         end
-
       end
 
       context 'when time_to_sleep set to invalid value' do
-
         it 'raises ArgumentError' do
-
-          expect { described_class.new(time_to_sleep: -1) }.to raise_error ArgumentError
-          
+          expect { described_class.new(time_to_sleep: -1) }.to(raise_error(ArgumentError))
         end
-
       end
 
       context 'when retriable set to invalid value' do
-
         it 'raises ArgumentError' do
-
-          expect { described_class.new(retriable: StandardError) }.to raise_error ArgumentError
-          
+          expect { described_class.new(retriable: StandardError) }.to(raise_error(ArgumentError))
         end
-
       end
 
       context 'when retry_proc set to invalid value' do
-
         it 'raises ArgumentError' do
-
-          expect { described_class.new(retry_proc: {}) }.to raise_error ArgumentError
-          
+          expect { described_class.new(retry_proc: {}) }.to(raise_error(ArgumentError))
         end
-
       end
 
       context 'when retry_condition_proc set to invalid value' do
-
         it 'raises ArgumentError' do
-
-          expect { described_class.new(retry_condition_proc: {}) }.to raise_error ArgumentError
-          
+          expect { described_class.new(retry_condition_proc: {}) }.to(raise_error(ArgumentError))
         end
-
       end
-      
     end
-
   end
-
-
 end
--- spec/take2_spec.rb
+++ spec/take2_spec.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-RSpec.describe Take2 do
-
+RSpec.describe(Take2) do
   let!(:config) do
     Take2.configure do |c|
       c.retries               = 1
       c.retriable             = [Net::HTTPServerException, Net::HTTPRetriableError].freeze
-      c.retry_condition_proc  = proc {false}
+      c.retry_condition_proc  = proc { false }
       c.retry_proc            = proc {}
       c.time_to_sleep         = 0.5
     end
@@ -15,148 +16,113 @@ RSpec.describe Take2 do
   let(:object)  { klass.new }
 
   describe 'default values' do
-
     subject { klass.retriable_configuration }
 
     it 'has a default value for :retries' do
-      expect(subject[:retries]).to eql described_class.configuration[:retries]
+      expect(subject[:retries]).to(eql(described_class.configuration[:retries]))
     end
 
     it 'has a default value for :retriable' do
-      expect(subject[:retriable]).to eql described_class.configuration[:retriable]
+      expect(subject[:retriable]).to(eql(described_class.configuration[:retriable]))
     end
 
     it 'has a default value for :retry_condition_proc' do
-      expect(subject[:retry_condition_proc].call).to eql described_class.configuration[:retry_condition_proc].call
+      expect(subject[:retry_condition_proc].call).to(eql(described_class.configuration[:retry_condition_proc].call))
     end
 
     it 'has a default value for :retry_proc' do
-      expect(subject[:retry_proc].call).to eql described_class.configuration[:retry_proc].call
+      expect(subject[:retry_proc].call).to(eql(described_class.configuration[:retry_proc].call))
     end
 
     it 'has a default value for :time_to_sleep' do
-      expect(subject[:time_to_sleep]).to eql described_class.configuration[:time_to_sleep]
+      expect(subject[:time_to_sleep]).to(eql(described_class.configuration[:time_to_sleep]))
     end
-
   end
 
   describe 'included class helpers' do
-
     subject { klass.retriable_configuration }
 
     describe '.number_of_retries' do
-
       context 'with valid argument' do
-
         it 'sets the :retries attribute' do
-          klass.number_of_retries 1
-          expect(subject[:retries]).to eql 1
+          klass.number_of_retries(1)
+          expect(subject[:retries]).to(eql(1))
         end
-
       end
 
       context 'with invalid argument' do
-
         it 'raises ArgumentError' do
-          expect { klass.number_of_retries 0 }.to raise_error ArgumentError
+          expect { klass.number_of_retries(0) }.to(raise_error(ArgumentError))
         end
-
       end
-
     end
 
     describe '.retriable_errors' do
-
       context 'with valid argument' do
-
         it 'sets the :retriable_errors attribute' do
           retriables = IOError
-          klass.retriable_errors retriables
-          expect(subject[:retriable]).to eql [retriables]
+          klass.retriable_errors(retriables)
+          expect(subject[:retriable]).to(eql([retriables]))
         end
-
       end
 
       context 'with invalid argument' do
-
         it 'raises ArgumentError' do
           class Klass; end
-          expect { klass.retriable_errors Klass }.to raise_error ArgumentError
+          expect { klass.retriable_errors(Klass) }.to(raise_error(ArgumentError))
         end
-
       end
-
     end
 
     describe '.retriable_condition' do
-
       context 'with valid argument' do
-
         it 'sets the :retriable_condition attribute' do
           retriable_proc = proc { 'Ho-Ho-Ho' }
-          klass.retriable_condition retriable_proc
-          expect(subject[:retry_condition_proc].call).to eql retriable_proc.call
+          klass.retriable_condition(retriable_proc)
+          expect(subject[:retry_condition_proc].call).to(eql(retriable_proc.call))
         end
-
       end
 
       context 'with invalid argument' do
-
-         it 'raises ArgumentError' do
-          expect { klass.retriable_condition Class.new }.to raise_error ArgumentError
-         end
-
+        it 'raises ArgumentError' do
+          expect { klass.retriable_condition(Class.new) }.to(raise_error(ArgumentError))
+        end
       end
-
     end
 
     describe '.on_retry' do
-
       context 'with valid argument' do
-
         it 'sets the :on_retry attribute' do
           retry_proc = proc { |el| el }
-          klass.on_retry retry_proc
-          expect(subject[:retry_proc].call).to eql retry_proc.call
+          klass.on_retry(retry_proc)
+          expect(subject[:retry_proc].call).to(eql(retry_proc.call))
         end
-
-      end  
-
-      context 'with invalid argument' do
-
-        it 'raises ArgumentError' do
-          expect { klass.on_retry Class.new }.to raise_error ArgumentError
-        end
-
       end
 
+      context 'with invalid argument' do
+        it 'raises ArgumentError' do
+          expect { klass.on_retry(Class.new) }.to(raise_error(ArgumentError))
+        end
+      end
     end
 
     describe '.sleep_before_retry' do
-
       context 'with valid argument' do
-
         it 'sets the :sleep_before_retry attribute' do
-          klass.sleep_before_retry 3.5
-          expect(subject[:time_to_sleep]).to eql 3.5
+          klass.sleep_before_retry(3.5)
+          expect(subject[:time_to_sleep]).to(eql(3.5))
         end
-
       end
-      
+
       context 'with invalid argument' do
-
         it 'raises ArgumentError' do
-          expect { klass.sleep_before_retry -1 }.to raise_error ArgumentError
+          expect { klass.sleep_before_retry(-1) }.to(raise_error(ArgumentError))
         end
-
       end
-
     end
-
   end
 
   describe '.call_api_with_retry' do
-
     def increment_retry_counter
       @tries += 1
     end
@@ -167,55 +133,72 @@ RSpec.describe Take2 do
     end
 
     context 'when raised with non retriable error' do
-
-      let(:error) { StandardError.new 'Release the Kraken!!' }
+      let(:error) { StandardError.new('Release the Kraken!!') }
 
       before(:each) { @tries = 0 }
 
       it 're raises the original error' do
         expect do
           object.call_api_with_retry { wrath_the_gods_with error }
-        end.to raise_error error.class
+        end.to(raise_error(error.class))
       end
 
       it 'is not retried' do
         expect do
-          object.call_api_with_retry { wrath_the_gods_with error } rescue nil
-        end.to change{@tries}.from(0).to(1)
+          object.call_api_with_retry { wrath_the_gods_with error }
+        rescue
+          nil
+        end.to(change { @tries }.from(0).to(1))
       end
 
       # it 'logs the error' do
       #   expect(object).to receive(:log_error).with(error)
       #   object.call_api_with_retry { wrath_the_gods_with error } rescue nil
       # end
-
     end
 
     context 'when raised with retriable error' do
-
-      let(:retriable_error) {  Net::HTTPRetriableError.new 'Release the Kraken...many times!!', nil }
+      let(:retriable_error) {  Net::HTTPRetriableError.new('Release the Kraken...many times!!', nil) }
 
       before(:each) { @tries = 0 }
 
       it 'retries correct number of times' do
         expect do
-          object.call_api_with_retry { wrath_the_gods_with retriable_error } rescue nil
-        end.to change {@tries}.from(0).to(klass.retriable_configuration[:retries] + 1)
+          object.call_api_with_retry { wrath_the_gods_with retriable_error }
+        rescue
+          nil
+        end.to(change { @tries }.from(0).to(klass.retriable_configuration[:retries] + 1))
       end
 
       it 'calls the retry proc' do
-        expect(klass.retriable_configuration[:retry_proc]).to receive(:call).exactly(klass.retriable_configuration[:retries])
-        object.call_api_with_retry { wrath_the_gods_with retriable_error } rescue nil
+        expect(klass.retriable_configuration[:retry_proc])
+          .to(receive(:call)
+          .exactly(klass.retriable_configuration[:retries]))
+        begin
+          object.call_api_with_retry { wrath_the_gods_with retriable_error }
+        rescue
+          nil
+        end
       end
 
       it 'calls the retry_condition proc' do
-        expect(klass.retriable_configuration[:retry_condition_proc]).to receive(:call).exactly(klass.retriable_configuration[:retries])
-        object.call_api_with_retry { wrath_the_gods_with retriable_error } rescue nil
+        expect(klass.retriable_configuration[:retry_condition_proc])
+          .to(receive(:call)
+          .exactly(klass.retriable_configuration[:retries]))
+        begin
+          object.call_api_with_retry { wrath_the_gods_with retriable_error }
+        rescue
+          nil
+        end
       end
 
       it 'sleeps the correct amount of time' do
-        allow_any_instance_of(Object).to receive(:sleep).with(klass.retriable_configuration[:time_to_sleep])
-        object.call_api_with_retry { wrath_the_gods_with retriable_error } rescue nil
+        allow_any_instance_of(Object).to(receive(:sleep).with(klass.retriable_configuration[:time_to_sleep]))
+        begin
+          object.call_api_with_retry { wrath_the_gods_with retriable_error }
+        rescue
+          nil
+        end
       end
 
       # it 'logs the error' do
@@ -226,55 +209,66 @@ RSpec.describe Take2 do
       it 're raises the original error' do
         expect do
           object.call_api_with_retry { wrath_the_gods_with retriable_error }
-        end.to raise_error retriable_error.class
+        end.to(raise_error(retriable_error.class))
       end
-
     end
 
     context 'with custom options' do
-
-      let(:retriable_error)     { Net::HTTPRetriableError.new 'Release the Kraken...many times!!', nil }
-      let(:new_retriable_error) { IOError.new 'You shall not PASS!' }
+      let(:retriable_error)     { Net::HTTPRetriableError.new('Release the Kraken...many times!!', nil) }
+      let(:new_retriable_error) { IOError.new('You shall not PASS!') }
 
       before(:each) { @tries = 0 }
-        
+
       it 'overwrites the :retries' do
         expect do
-          object.call_api_with_retry(retries: 3) { wrath_the_gods_with retriable_error } rescue nil
-        end.to change { @tries }.from(0).to(4)
+          object.call_api_with_retry(retries: 3) { wrath_the_gods_with retriable_error }
+        rescue
+          nil
+        end.to(change { @tries }.from(0).to(4))
       end
 
       it 'overwrites the :retry_proc' do
         new_proc = proc { 1**1 }
-        expect(new_proc).to receive(:call).exactly(klass.retriable_configuration[:retries])
-        object.call_api_with_retry(retry_proc: new_proc) { wrath_the_gods_with retriable_error } rescue nil          
+        expect(new_proc).to(receive(:call).exactly(klass.retriable_configuration[:retries]))
+        begin
+          object.call_api_with_retry(retry_proc: new_proc) { wrath_the_gods_with retriable_error }
+        rescue
+          nil
+        end
       end
 
       it 'overwrites the :retry_condition_proc' do
         new_proc = proc { true }
-        expect(new_proc).to receive(:call).exactly(klass.retriable_configuration[:retries])
-        object.call_api_with_retry(retry_condition_proc: new_proc) { wrath_the_gods_with retriable_error } rescue nil          
+        expect(new_proc).to(receive(:call).exactly(klass.retriable_configuration[:retries]))
+        begin
+          object.call_api_with_retry(retry_condition_proc: new_proc) { wrath_the_gods_with retriable_error }
+        rescue
+          nil
+        end
       end
 
       it 'overwrites the :time_to_sleep' do
-        allow_any_instance_of(Object).to receive(:sleep).with(1.66)
-        object.call_api_with_retry(time_to_sleep: 1.66) { wrath_the_gods_with retriable_error } rescue nil
+        allow_any_instance_of(Object).to(receive(:sleep).with(1.66))
+        begin
+          object.call_api_with_retry(time_to_sleep: 1.66) { wrath_the_gods_with retriable_error }
+        rescue
+          nil
+        end
       end
 
       it 'overwrites the :retriable' do
-        expect do                
-          object.call_api_with_retry(retriable: [new_retriable_error]) { wrath_the_gods_with retriable_error } rescue nil
-        end.to change { @tries }.from(0).to(1)
+        expect do
+          object.call_api_with_retry(retriable: [new_retriable_error]) { wrath_the_gods_with retriable_error }
+        rescue
+          nil
+        end.to(change { @tries }.from(0).to(1))
       end
 
       it 'raises ArgumentError if there are invalid keys' do
-        expect do                
+        expect do
           object.call_api_with_retry(invalid_key: :nope) { wrath_the_gods_with retriable_error }
-        end.to raise_error ArgumentError
+        end.to(raise_error(ArgumentError))
       end
-
     end
-
   end
-
 end
