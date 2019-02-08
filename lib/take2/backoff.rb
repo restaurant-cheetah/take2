bdd0362647e8b6f21c3ec9cb6b6fef31b3820035
Anton Magids <evilnomadx@gmail.com>
2019-02-08 16:43:05 +0200

    Add backoff class with three strategies

---
+++ lib/take2/backoff.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Take2
+  class Backoff
+    attr_reader :type, :start, :retries, :factor, :intervals
+    def initialize(type, start = 1, factor = 1, retries = 10)
+      @type = type
+      @start = start
+      @retries = retries
+      @factor = factor
+      @intervals = intervals_table
+    end
+
+    private
+
+    def intervals_table
+      send(type)
+    end
+
+    def linear
+      (start...(retries + start)).map { |i| i * factor }
+    end
+
+    def fibonachi
+      (1..20).map { |i| fibo(i) }.partition { |x| x >= start }.first.take(retries)
+    end
+
+    def exponential
+      (1..20).each_with_index.inject([]) do |memo, (el, ix)|
+        memo << if ix == 0
+          start
+        else
+          (2**el - 1) + rand(1..2**el)
+        end
+      end.take(retries)
+    end
+
+    def fibo(n, memo = {})
+      return n if n < 2
+      memo[n] ||= fibo(n - 1, memo) + fibo(n - 2, memo)
+    end
+  end
+end
